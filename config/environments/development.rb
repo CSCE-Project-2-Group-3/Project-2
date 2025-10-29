924eb9fb3d16e7132ae42019348cf6d41282e7e5
Amesh537 <t537000707@tamu.edu>
2025-10-29 13:27:02 -0500

    Added Gmail back

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,16 +46,16 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   # Use Gmail SMTP for sending emails
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:              "smtp.gmail.com",
-  #   port:                 587,
-  #   domain:               "gmail.com",
-  #   user_name:            Rails.application.credentials[:gmail][:user_name],
-  #   password:             Rails.application.credentials[:gmail][:password],
-  #   authentication:       :plain,
-  #   enable_starttls_auto: true
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "gmail.com",
+    user_name:            Rails.application.credentials[:gmail][:user_name],
+    password:             Rails.application.credentials[:gmail][:password],
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
