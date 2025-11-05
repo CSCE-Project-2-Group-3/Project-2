3e4feec3a06109ca08ccb1e1814d0a0544a95941
PRADEEPPERIYASAMYTAMU <papradeep11cs@tamu.edu>
2025-11-04 18:59:59 -0600

    Fix: Change participant_ids to array type

--- db/migrate/20251105005228_change_participant_ids_to_array_in_expenses.rb
+++ db/migrate/20251105005228_change_participant_ids_to_array_in_expenses.rb
@@ -1,5 +1,12 @@
 class ChangeParticipantIdsToArrayInExpenses < ActiveRecord::Migration[8.0]
   def change
-    change_column :expenses, :participant_ids, :integer, array: true, default: [], using: 'ARRAY[participant_ids]::INTEGER[]'
+    change_column_default :expenses, :participant_ids, nil
+
+    # Step 2: Change the column type and convert all existing data.
+    # The USING clause converts the old single integer (e.g., 5) into an array ([5]).
+    change_column :expenses, :participant_ids, :integer, array: true, using: 'ARRAY[participant_ids]::INTEGER[]'
+
+    # Step 3: Now that the column is an array, set the new default value.
+    change_column_default :expenses, :participant_ids, from: nil, to: []
   end
 end
